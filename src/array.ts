4da14e43b8b4c25b9ca3733b0d27014daccb7cec
jaskang <jaskang@qq.com>
2023-04-12 17:10:29 +0800

    fix: export interate

--- src/array.ts
+++ src/array.ts
@@ -10,7 +10,11 @@
  * @param initValue - 初始值
  * @returns
  */
-function iterate<T>(count: number, func: (currentValue: T, iteration: number) => T, initValue: T) {
+export function iterate<T>(
+  count: number,
+  func: (currentValue: T, iteration: number) => T,
+  initValue: T
+) {
   let value = initValue
   for (let i = 1; i <= count; i++) {
     value = func(value, i)
