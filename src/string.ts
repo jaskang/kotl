59bf6a949c0aa51fb6adb617b48f34e2b3cc3462
jaskang <jaskang@qq.com>
2023-07-06 15:23:03 +0800

    feat: slash

--- src/string.ts
+++ src/string.ts
@@ -2,10 +2,19 @@
  * 去除字符串两端的空格
  * @param str - 要处理的字符串
  * @param charsToTrim - 要去除的字符，默认为空格
- * @returns
+ * @returns string
  */
 export const trim = (str: string | null | undefined, charsToTrim: string = ' ') => {
   if (!str) return ''
   const regex = new RegExp(`^[${charsToTrim}]+|[${charsToTrim}]+$`, 'g')
   return str.replace(regex, '')
+}
+
+/**
+ * 将 windows 路径字符串中的 '\\' 转换为 '/'
+ * @param p 路径字符串
+ * @returns string
+ */
+export function slash(p: string): string {
+  return p.replace(/\\/g, '/')
 }
