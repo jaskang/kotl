aa0c2a0eae491a7677c0200cd39f7f45dc734a94
jaskang <jaskang@qq.com>
2023-07-19 15:16:45 +0800

    fix: isBrowser is func

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,5 @@
 /**
  * 判断是否在浏览器环境下运行
  */
-export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined'
+export const isBrowser = () =>
+  typeof window !== 'undefined' && typeof window.document !== 'undefined'
--- src/node.ts
+++ src/node.ts
@@ -1,4 +1,5 @@
 /**
  * 判断是否在 Node 环境中
  */
-export const isNode = typeof process !== "undefined" && process.versions != null && process.versions.node != null;
+export const isNode = () =>
+  typeof process !== 'undefined' && process.versions != null && process.versions.node != null
