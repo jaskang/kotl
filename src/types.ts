987f4a18c1471c82b214f66765b0f2c96a2c1641
jaskang <jaskang@qq.com>
2023-07-24 10:38:39 +0800

    feat: Flat

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,7 @@ export type AugmentedRequired<T extends object, K extends keyof T = keyof T> = O
   Required<Pick<T, K>>
 
 export type Func = (...args: any[]) => any
+
+export type Data = Record<string, any>
+
+export type Flat<T> = T extends Func ? T : T extends object ? { [K in keyof T]: T[K] } : T
