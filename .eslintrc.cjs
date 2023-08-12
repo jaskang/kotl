adc11a3f3b4babf456102223bd47e9c10d442f54
JasKang <jaskang@qq.com>
2023-08-12 12:34:37 +0800

    chore: prettier

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@jaskang/eslint-config'],
-  root: true,
-  rules: {
-  },
-}
--- .prettierrc.cjs
+++
@@ -1,3 +0,0 @@
-const prettier = require('@jaskang/config/prettier')
-
-module.exports = prettier
