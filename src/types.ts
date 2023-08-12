ecb8d693970271e964bcf96560c4a74e8f27f6df
JasKang <jaskang@qq.com>
2023-08-12 12:31:26 +0800

    feat: type Expand

--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,14 @@ export type Data = Record<string, any>
 
 export type Flat<T> = T extends Func ? T : T extends object ? { [K in keyof T]: T[K] } : T
 
-export type Prettify<T> = { [K in keyof T]: T[K] } & {}
+export type Expand<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: O[K] }
+    : never
+  : T
+
+export type ExpandDeep<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: Expand<O[K]> }
+    : never
+  : T
